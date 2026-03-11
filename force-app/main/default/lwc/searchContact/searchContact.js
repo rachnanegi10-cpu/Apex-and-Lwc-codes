2c02149644b5022855f14eeca35fbca6edd229d3
Rachna <rachnanegi.10@gmail.com>
2026-03-11 16:36:33 +0530

    search contact and opportunity by account name

--- force-app/main/default/lwc/searchContact/searchContact.js
+++ force-app/main/default/lwc/searchContact/searchContact.js
@@ -1,23 +1,34 @@
 import { LightningElement,api,wire,track } from 'lwc';
 import getContact from '@salesforce/apex/searchContact.getContact';
+import getOpps from '@salesforce/apex/searchContact.getOpportunity';
 const COLUMNS = [
     { label: 'Name', fieldName: 'Name' },
     { label: 'Email', fieldName: 'Email', type: 'email' },
     { label: 'Phone', fieldName: 'Phone', type: 'phone' }
 ];
 
+const COLUMNS2 = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Stage Name', fieldName: 'StageName', type: 'emaPicklistil' },
+    { label: 'Type', fieldName: 'Type', type: 'Picklist' }
+];
+
 export default class SearchContact extends LightningElement {
     @track contacts;
     @track error;
+    @track opps;
     columns=COLUMNS;
+    columns2=COLUMNS2;
     selectedAccountId;
 
     handleAccountChange(event){
         this.selectedAccountId=event.detail.recordId;
         if(this.selectedAccountId){
             this.loadContacts();
+            this.loadOpps();
         }else{
             this.contacts=undefined;
+            this.opps=undefined;
             this.error=undefined;
         }
     }
@@ -32,6 +43,20 @@ export default class SearchContact extends LightningElement {
                     .catch(error=>{
                         this.error=error;
                         this.contacts=undefined;
+                    });
+
+    }
+
+    loadOpps(){
+
+        getOpps({accountId:this.selectedAccountId})
+                    .then(result=>
+                        {   
+                            this.opps=result; 
+                            this.error=undefined;})
+                    .catch(error=>{
+                        this.error=error;
+                        this.opps=undefined;
                     });
 
     }
